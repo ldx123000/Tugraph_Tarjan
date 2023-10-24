8ba4916c69943edb5f508923983395f0b6d46be3
toeveryone <76545384+toeveryone@users.noreply.github.com>
2023-10-24 15:12:45 +0800

    Update tarjan_core.cpp

--- procedures/algo_cpp/tarjan_core.cpp
+++ procedures/algo_cpp/tarjan_core.cpp
@@ -10,7 +10,7 @@ void TARJANCore(OlapBase<Empty> &graph, size_t u, tar_pack &pack) {
     pack.low[u] = pack.para_datas[DEEP];
     pack.flag[u] = 1;
     pack.a[++pack.para_datas[TOP]] = u;
-    for (auto &edge : graph.OutEdges(u)) {
+    for (auto &edge : graph.OutEdges(u)) {//使用tugraph api遍历每个出节点
         size_t dst = edge.neighbour;
         printf("dst:%ld\n", dst);
         if (pack.dfn[dst] == 0) {
@@ -19,7 +19,7 @@ void TARJANCore(OlapBase<Empty> &graph, size_t u, tar_pack &pack) {
         } else if (pack.flag[dst])
             pack.low[u] = min<int>(pack.low[u], pack.low[dst]);
     }
-    if (pack.dfn[u] == pack.low[u]) {
+    if (pack.dfn[u] == pack.low[u]) { //找到一个强连通分量，将该强连通分量所包含的节点信息进行处理
         pack.color[u] = ++pack.para_datas[SUM];
         pack.map[pack.para_datas[SUM]].push_back(u);
         printf("map:%ld %ld %ld\n", pack.para_datas[SUM],
